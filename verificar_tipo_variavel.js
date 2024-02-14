4d9c39eb2b73a2d49eaaff87095f1bb5e05fe218
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2024-02-14 11:14:26 -0300

    Add files via upload

---
+++ verificar_tipo_variavel.js
@@ -0,0 +1,5 @@
+var x = 5;
+console.log(typeof x === "number"); // number
+console.log(typeof 10 === "number"); // number
+console.log(typeof "10" === "string"); // string
+console.log(typeof [1,2] === "object"); // object
