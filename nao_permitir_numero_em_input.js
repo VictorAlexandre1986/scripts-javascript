6e8dd01649e5577d416432a71bc41adadeaba354
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2024-04-16 11:36:44 -0300

    Add files via upload

---
+++ nao_permitir_numero_em_input.js
@@ -0,0 +1,8 @@
+var inputNome = document.querySelector("#nome");
+nome.addEventListener("keypress", function(e) {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+  
+  if (keyCode > 47 && keyCode < 58) {
+    e.preventDefault();
+  }
+});
